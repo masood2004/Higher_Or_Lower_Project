499f030425f7330e1aa77b99649341cf651c48fb
Syed Masood Hussain <hmasood3288@gmail.com>
2024-12-07 02:20:03 +0500

    Complete Higher Lower Project/Game

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import random
+from art import logo, vs
+from game_data import data
+
+print(logo)
+
+def random_account():
+    """Select the random profile/account from the data."""
+    account = random.choice(data)
+    return account
+
+score = 0
+
+celebrity_1 = random_account()
+celebrity_2 = random_account()
+
+while True:
+    while celebrity_1 == celebrity_2:
+        celebrity_2 = random_account()
+
+    print(f"Compare A: {celebrity_1['name']}, a {celebrity_1['description']}, from {celebrity_1['country']}")
+    print(vs)
+    print(f"Against B: {celebrity_2['name']}, a {celebrity_2['description']}, from {celebrity_2['country']}")
+
+    celebrity_1_followers = celebrity_1['follower_count']
+    celebrity_2_followers = celebrity_2['follower_count']
+
+    ask_user = input("Who has more followers? Type 'A' or 'B', or 'q' to quit: ").lower()
+
+    if ask_user == 'q':
+        print(f"Game over! Your final score is {score}.")
+        break
+
+    if ask_user == "a":
+        if celebrity_1_followers > celebrity_2_followers:
+            print("You guessed it correctly!")
+            score += 1
+            print(f"Current score: {score}")
+            celebrity_1 = celebrity_2
+            celebrity_2 = random_account()
+        else:
+            os.system("cls")
+            print(logo)
+            print("You guessed it incorrectly!")
+            print(f"Your final score is {score}.")
+            input()
+            break
+    elif ask_user == "b":
+        if celebrity_2_followers > celebrity_1_followers:
+            print("You guessed it correctly!")
+            score += 1
+            print(f"Current score: {score}")
+            celebrity_2 = celebrity_1
+            celebrity_1 = random_account()
+        else:
+            os.system("cls")
+            print(logo)
+            print("You guessed it incorrectly!")
+            print(f"Your final score is {score}.")
+            input()
+            break
